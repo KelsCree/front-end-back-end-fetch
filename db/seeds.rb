d105849e2ab863109c3e1950522eb72503f1ad2e
KelsCree <kelseycreehan@gmail.com>
2020-09-01 08:53:12 -0600

    Frontend Backend Practice with Ahmed.

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Bird.create(name: "Smokey" , species: "Red Harson")
+Bird.create(name: "Fluffy" , species: "Grey Fox")
+Bird.create(name: "Cheesy" , species: "Yellow Hound")
